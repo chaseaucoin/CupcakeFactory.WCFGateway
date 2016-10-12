6753b3fd9760a67e2973e755b0965e98b9721db8
Chase Q. Aucoin <chase@cupcakefactory.net>
2016-10-11 23:22:40 -0500

    Add project files.

---
+++ CupcakeFactory.WCFGateway/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CupcakeFactory.WCFGateway
+{
+    public class Class1
+    {
+    }
+}
